38543bbfe8765b028f1f1a71a5022a4c94eec157
AdeleEmma <144701648+AdeleEmma@users.noreply.github.com>
2023-10-29 16:16:54 +0200

    Create  function harjutus7.js

---
+++ js_harjutused/function harjutus7.js
@@ -0,0 +1,10 @@
+function printIntegersFrom100to1() {
+  let number = 100;
+  while (number >= 1) {
+    console.log(number);
+    number--;
+  }
+}
+
+// Call the function to print the integers
+printIntegersFrom100to1();
