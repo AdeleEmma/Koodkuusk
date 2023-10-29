356b9dddd4764c20b2ec434398c0c38c922fd5ce
AdeleEmma <144701648+AdeleEmma@users.noreply.github.com>
2023-10-29 16:19:19 +0200

    Create function harjutus10.js

---
+++ js_harjutused/function harjutus10.js
@@ -0,0 +1,7 @@
+const myNameComponents = ['Samuel', 'L', 'Jackson'];
+
+// Ühenda massiivi elemendid stringiks ja loo lause
+const fullName = myNameComponents.join(' ');
+
+// Prindi lause konsooli
+console.log(`Minu nimi on ${fullName}.`);
