a4218c1c196966028af2f85f9c50905d52ce7364
AdeleEmma <144701648+AdeleEmma@users.noreply.github.com>
2023-12-18 17:23:50 +0200

    Update Index.js

--- Veebisait/Index.js
+++ Veebisait/Index.js
@@ -1,1 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+// Staatilise sisu jaoks kasuta public kausta
+app.use(express.static('public'));
+
+// Avaleht
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// About
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/public/about.html');
+});
+
+// Kontakt
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/public/contact.html');
+});
+
+// Kuula serverit portil 3000
+app.listen(port, () => {
+  console.log(`Server kuulab portil ${port}`);
+});
