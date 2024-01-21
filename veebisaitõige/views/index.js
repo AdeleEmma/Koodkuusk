57218d4ff86319fd3ed91c502eefccaf3173c64c
AdeleEmma <144701648+AdeleEmma@users.noreply.github.com>
2024-01-21 20:10:59 +0200

    Create index.js

---
+++ veebisaitõige/views/index.js
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>Paul Keres</title>
+    <link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/styles.css">
+    <style>
+        .media-container {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            overflow: hidden;
+        }
+        .media-container img {
+            max-height: calc(100vh - 180px);
+            margin-right: 20px;
+        }
+        .media-container div {
+            overflow: auto;
+        }
+        .container {
+            padding: 10px;
+            flex-grow: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <div class="header-container">
+            <h1>Paul Keres</h1>
+            <nav>
+                <ul>
+                    <li><a href="/about">Info</a></li>
+                    <li><a href="/contact">Kontakt</a></li>
+                    <li><a href="/estonian-news">Eesti Uudised</a></li>
+
+                </ul>
+            </nav>
+            <a href="/profile"class="profile-link"> <%= user.username %></a>
+        </div>
+    </header>
+    
+    <div class="container">
+        <div class="media-container">
+            <img src="https://t3.gstatic.com/licensed-image?q=tbn:ANd9GcQZmWcZ3WsOC3vFTJWowIOyrgfaLISsoFADrCxMIh1fu_9TzAwk8Z8fzt63bgclfHi5" alt="Paul Keres" width="400">
+            <div>
+                <p>Paul Keres (1916 - 1975) oli Eesti malesuurmeister ja malekirjanik. Keres on Eesti spordi suurkuju, kelle tähtsus ja tähendus ulatuvad üle malemängu piiride. </p> <p>Keres oli silmapaistev Eesti malesuurmeister ja malekirjanik, keda peetakse üheks kõigi aegade suurimaks malemängijaks. Tema panus malemaailma ja Eesti spordiajalukku on hindamatu ning tema nimi on sünonüüm tipptasemel malega.</p>
+
+                <p>Paul Kerese andekus males avaldus varases nooruses ning ta kiiresti tõusis Eesti malesõprade ja -mängijate seas kõrges hinnangu. Juba 1935. aastal, 19-aastasena, tuli ta Eesti meistriks, saavutades kiiresti tunnustuse mitmel rahvusvahelisel võistlusel. Tema karjäär oli täis muljetavaldavaid võite ja saavutusi.</p>
+    
+                <p>Üks tema suurimaid saavutusi oli tema panus Eesti male koondisele Maleolümpial. Paul Keres mängis Eesti eest 11 korral ja tuli olümpiamängudel koguni kolm korda meeskondliku male maailmameistriks.</p>
+    
+                <p>Tema mängustiil oli tuntud teravuse ja loomingulisuse poolest. Ta oli kuulus oma sügava avatud mängu mõistmise ja keerukate kombinatsioonide loomise poolest, mis on jätnud sügava jälje male ajalukku.</p>
+    
+                <p>Paul Keres on Eesti spordi suurkuju, kelle tähtsus ja tähendus ulatuvad üle malemängu piiride. Tema andekus, pühendumine ja panus spordimaailma on teinud temast igavese malelegendi.</p>
+    
+                <h2>Tunnustus</h2>
+                <ul> 
+                    <li>1959, 1962 - aasta sportlane</li>
+                    <li>1964 - Kalevi auliige</li>
+                    <li>1965 - Eesti NSV teeneline sporditegelane</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body> 
+</html>
